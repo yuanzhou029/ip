6ccfec2175f396a908f20ac0f127dc9fb7744fa5
yuanzhou029 <129774416+yuanzhou029@users.noreply.github.com>
2024-04-27 15:50:30 +0800

    Update query_japan_ips.py

--- query_japan_ips.py
+++ query_japan_ips.py
@@ -1,50 +1,46 @@
 import os
 import requests
-import json
-import geoip2.database
-from git import Repo
 
-def get_japan_ips():
-    # 使用API查询IP地址
-    response = requests.get("https://ipdb.api.030101.xyz/?type=cfv4;proxy&down=true")
-    ips = response.text.split('\n')
+def query_ip_info(ip):
+    # 构建 API 请求 URL
+    api_url = f"http://ip-api.com/json/{ip}?lang=zh-CN"
 
-    # 加载GeoIP2数据库
-    reader = geoip2.database.Reader('GeoLite2-Country.mmdb')
+    # 发送 GET 请求
+    response = requests.get(api_url)
 
-    japan_ips = []
+    # 解析 JSON 响应
+    data = response.json()
 
-    # 查询IP地址的地理位置，如果是日本则添加到列表中
-    for ip in ips:
-        try:
-            response = reader.country(ip)
-            if response.country.iso_code == 'JP':
-                japan_ips.append(ip)
-        except:
-            pass
+    # 提取地理信息中的国家
+    country = data.get("country", "未知")
 
-    return japan_ips
+    return country
 
-def save_to_file(ips, filename):
+def save_japan_ips_to_file(ips, filename):
+    # 将获取到的日本 IP 地址写入文件
     with open(filename, 'w') as file:
         for ip in ips:
-            file.write(f"{ip} # 日本\n")
+            file.write(f"{ip}\n")
 
 def commit_and_push(filename):
-    # 获取存储库根目录
-    repo_dir = os.getcwd()
-
-    # 切换到存储库根目录
-    os.chdir(repo_dir)
-
-    # 使用GitPython库进行提交和推送
-    repo = Repo(repo_dir)
-    repo.git.add(filename)
-    repo.index.commit("Update Japan IPs")  # 提交更改
-    origin = repo.remote('origin')
-    origin.push()  # 推送更改到远程存储库
+    # 提交并推送文件到Git仓库
+    os.system(f"git add {filename}")
+    os.system(f'git commit -m "Update {filename}"')
+    os.system("git push")
 
 if __name__ == "__main__":
-    japan_ips = get_japan_ips()
-    save_to_file(japan_ips, 'japan_ips.txt')
+    # 读取存储库中的 ip.txt 文件
+    with open("ip.txt", "r") as file:
+        ips = file.readlines()
+    
+    # 去除换行符
+    ips = [ip.strip() for ip in ips]
+
+    # 查询IP地址的地理信息，并筛选出日本的IP地址
+    japan_ips = [ip for ip in ips if query_ip_info(ip) == "日本"]
+
+    # 将日本的IP地址保存到文件中
+    save_japan_ips_to_file(japan_ips, 'japan_ips.txt')
+
+    # 提交并推送文件到Git仓库
     commit_and_push('japan_ips.txt')
