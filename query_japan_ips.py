ca71e2e192ad4696623e5b264c21d9dc748cf587
yuanzhou029 <129774416+yuanzhou029@users.noreply.github.com>
2024-04-27 17:52:50 +0800

    Update query_japan_ips.py

--- query_japan_ips.py
+++ query_japan_ips.py
@@ -1,38 +1,36 @@
 import os
 import requests
-import json
 import geoip2.database
 from git import Repo
 
-def get_country_ips(country_code):
+def get_ips_by_country(country_code):
     # 使用API查询IP地址
-    response = requests.get(f"https://ipdb.api.030101.xyz/?type=cfv4;proxy;country={country_code}&down=true")
+    response = requests.get(f"https://ipdb.api.030101.xyz/?type=cfv4;proxy&down=true&country={country_code}")
     ips = response.text.split('\n')
+
     return ips
 
-def save_ips_to_file(ips, country_code, filename):
+def save_to_file(ips, filename):
     with open(filename, 'a') as file:
         for ip in ips:
-            file.write(f"{ip} #{country_code}\n")
+            file.write(f"{ip}\n")
 
 def clear_and_commit(filename):
-    # 删除指定文件
-    os.remove(filename)
+    # 删除 ip.txt 文件
+    os.remove('ip.txt')
 
     # 提交和推送到远程存储库
     repo_dir = os.getcwd()
     repo = Repo(repo_dir)
     repo.git.add('--all')
-    repo.index.commit(f"Update {filename} and delete ip.txt")
+    repo.index.commit("Update japan_ips.txt and delete ip.txt")
     origin = repo.remote('origin')
     origin.push()
 
 if __name__ == "__main__":
-    # 获取不同国家的IP地址列表并保存到文件中
-    countries = ['JP', 'KR', 'HK', 'TW', 'SG', 'VN']
+    countries = ['JP', 'KR', 'HK', 'TW', 'SG', 'VN']  # 需要获取IP的国家代码列表
     for country_code in countries:
-        country_ips = get_country_ips(country_code)
-        save_ips_to_file(country_ips, country_code, 'japan_ips.txt')
-
-    # 清除并提交 ip.txt 文件
+        ips = get_ips_by_country(country_code)
+        save_to_file(ips, 'japan_ips.txt')
+    
     clear_and_commit('ip.txt')
