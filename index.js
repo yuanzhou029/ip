58f9c64abf57d120d8b3a921399af6df89c36743
拉磨的驴 <129774416+yuanzhou029@users.noreply.github.com>
2026-06-25 08:22:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {
 
 const UUID = 'a0e7ce2a-4527-47a5-aeb7-d5b89c016aa8';
 // 反向代理IP，无法访问时通过代理访问
-const DEFAULT_PROXY_IP = 'jp.toi.cc.cd','hk.toi.cc.cd','us.toi.cc.cd'; // 来源：https://ipdb.030101.xyz/bestdomain/
+const DEFAULT_PROXY_IP = 'jp.toi.cc.cd'; // 来源：https://ipdb.030101.xyz/bestdomain/
 // 优选域名/IP
 const BEST_DOMAINS = ['ipv6.yh-iot.cloudns.org:443', 'cf.877774.xyz:443', '9888k.com:443', "cf.090227.xyz:443", "cdns.doon.eu.org:443", "sub.danfeng.eu.org:443", "cf.zhetengsha.eu.org:443", "japan.com:443"]
 
